ac78f248ddd464272a015902039656f0521b80b6
Hendrick Bahr <hendrick2bahr@gmail.com>
2022-10-01 16:00:43 -0300

    Modificando jokenpo

--- Jokenpo1.c
+++ Jokenpo1.c
@@ -9,39 +9,18 @@ int main(){
 
 
     int n1,n2,n3;
-    char x[50],y[50];
 
     while(n1 > 5){
 
-
         int r = (rand() % 3) + (1);
-        printf("Faça sua escolha 1.Pedra 2.Papel 3.Tesoura\n");
+        printf("Faca sua escolha 1.Pedra 2.Papel 3.Tesoura\n");
         scanf("%d", &n2);
         while(n2 != 1 && n2 != 2 && n2 != 3){
-        printf("Faça uma escolha valida 1.Pedra 2.Papel 3.Tesoura\n");
+        printf("Faca uma escolha valida 1.Pedra 2.Papel 3.Tesoura\n");
         scanf("%d", &n2);
         }
-
-        if(r == 1){
-            x == "Pedra";
-        }else if(r == 2){
-            x == "Papel";
-        }else{
-            x == "Tesoura";
-        }
-
-        if(n2 == 1){
-            y == "Pedra";
-            printf("%s", y);
-        }else if(n2 == 2){
-            y == "Papel";
-        }else{
-            y == "Tesoura";
-        }
-
-
         if(n2 == 1 && r == 2){
-            printf("Voce perdeu Sua jogada:%s Jogada do oponente:%s", y, x);
+            printf("Voce perdeu");
         }else if(n2 == 1 && r == 3){
             printf("Voce ganhou");
         }else if(n2 == 1 && r == 1){
